b5005be406cf051fc90688564a44d946e4768b3f
AlexanderSlokov <thanhzeus2016@gmail.com>
2023-09-04 21:17:33 +0700

    commit: add footer

--- home.php
+++ home.php
@@ -7,8 +7,8 @@
     <title>home</title>
 
     <!-- swiper css link  -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
-/>
+    <link href=" https://cdn.jsdelivr.net/npm/swiper@10.2.0/swiper-bundle.min.css " rel="stylesheet">
+    />
     <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
@@ -27,13 +27,13 @@
     <a href="home.php">home</a>
     <a href="about.php">about</a>
     <a href="package.php">package</a>
-    <a href="booking.php">booking</a>
+    <a href="booking.php">book</a>
 
 </nav>
 
 <!-- menu button  -->
 <div id="menu-btn" class="fas fa-bars"></div>
-
+</section>
 <!-- header section end -->
 
 
@@ -41,15 +41,68 @@
 
 
 
+<!--footer section starts-->
+<section class="footer">
+    
+    <div class="box-container">
+        
+        <div class="box">
+            <h3>Quick links</h3>
+            <a href="home.php"><i class="fas fa-angle right"></i>home</a>
+            <a href="about.php"><i class="fas fa-angle right"></i>about</a>
+            <a href="package.php"><i class="fas fa-angle right"></i>package</a>
+            <a href="booking.php"><i class="fas fa-angle right"></i>book</a>
+        </div>
+
+        
+        <div class="box">
+            
+            <h3>Extra links</h3>
+            
+            <a href="#"><i class="fas fa-angle right"></i>Ask Questions</a>
+            <a href="#"><i class="fas fa-angle right"></i>About Us</a>
+            <a href="#"><i class="fas fa-angle right"></i>Privacy Policy</a>
+            <a href="#"><i class="fas fa-angle right"></i>Terms of Use</a>
+            
+        </div>
+
+        <div class="box">
+
+            <h3>Contact Information</h3>
+
+            <a href="#"><i class="fas fa-phone"></i> +84 0123 456 789 </a>
+            <a href="#"><i class="fas fa-envelope"></i> dinhtandung.work@gmail.com </a>
+            <a href="#"><i class="fas fa-map"></i> Di An city, Binh Duong province, Vietnam </a>
+
+        </div>
+
+        <div class="box">
+            
+            <h3>Follow Us On:</h3>
+            
+            <a href="#"><i class="fab fa-facebook-f"></i> FACEBOOK </a>
+            <a href="#"><i class="fab fa-twitter"></i> TWITTER </a>
+                <a href="#"><i class="fab fa-instagram"></i> INSTAGRAM </a>
+            <a href="#"><i class="fab fa-linkedin"></i> LINKEDIN </a>
+        </div>
+        
+    </div>
+
+    <div class="credit">Created by following instructions of mr.WebDesigner <span>Dinh Tan Dung</span> </div>
+</section>
+<!--footer section ends-->
 
 
 
 
 
 
-    <!-- siwper js link script  -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
-    <!-- custom js file link script -->
-    <script src="js/script.js"></script>
+
+<!-- swiper js link script  -->
+<script src=" https://cdn.jsdelivr.net/npm/swiper@10.2.0/swiper-bundle.min.js "></script>
+
+<!-- custom js file link script -->
+<script src="js/script.js"></script>
+
 </body>
 </html>
