584d41bd608ed7483dfbbdd6683bfa82eb57de1c
AlexanderSlokov <thanhzeus2016@gmail.com>
2023-09-04 15:13:47 +0700

    Init commit

---
+++ home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
+/>
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<!-- header section start -->
+
+<section class="header">
+
+<a href="home.php" class="logo">TRAVEL</a>
+
+<nav class="navigationBar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="booking.php">booking</a>
+
+</nav>
+
+<!-- menu button  -->
+<div id="menu-btn" class="fas fa-bars"></div>
+
+<!-- header section end -->
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- siwper js link script  -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <!-- custom js file link script -->
+    <script src="js/script.js"></script>
+</body>
+</html>
